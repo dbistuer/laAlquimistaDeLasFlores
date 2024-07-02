2eb85f5e8ded74a81baa099c22aa8b0efeb0faff
Dani <dbistuer@msn.com>
2024-07-02 12:49:27 +0200

    adding environments for release and develop

---
+++ laAlquimistaDeLasFlores/settings/release.py
@@ -0,0 +1,13 @@
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+
+DATABASES = {
+
+}
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
