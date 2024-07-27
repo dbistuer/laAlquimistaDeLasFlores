c2929f942288e376c523e6de44afb1ba87fb3ec1
Dani <dbistuer@msn.com>
2024-07-27 23:09:40 +0200

    selenium e2e tests user

--- laAlquimistaDeLasFlores/settings/test.py
+++ laAlquimistaDeLasFlores/settings/test.py
@@ -7,7 +7,7 @@ from .base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'test.sqlite3',
     }
 }
 
